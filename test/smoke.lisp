0284bd86dfc19827cea26191f224f7624a631b92
Victor <anyakinvictor@yahoo.com>
2013-05-09 15:08:49 +0300

    added simple site page and more complex tests code

--- test/smoke.lisp
+++ test/smoke.lisp
@@ -1,5 +1,5 @@
 
-;; sbcl --load smoke.lisp
+;; sbcl --load smoke.lisp --quit
 
 (ql:quickload :cl-string-match)
 (ql:quickload :lisp-unit)
@@ -7,11 +7,21 @@
 (in-package :cl-string-match)
 (use-package :lisp-unit)
 
+(setq *print-failures* t)
+
+(defparameter *funcs*
+  '(string-contains-brute
+    string-contains-bm))
+
+(defmacro run-assertions (val needle haystack)
+  (loop :for func :in *funcs*
+     :collect `(assert-equal ,val (,func ,needle ,haystack))))
+
 (define-test basic-test
-    (assert-equal 1 (string-contains-brute "a" "-a-")))
+    (run-assertions 0 "a" "a--")
+    (run-assertions 1 "a" "-a-")
+  (run-assertions 2 "a" "--a"))
 
 (run-tests :all)
-
-(cl:quit)
 
 ;; EOF
