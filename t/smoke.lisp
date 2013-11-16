f9bb5cc2d2561f968075b8fbbd424f22ea4a064a
Victor <609508+vityok@users.noreply.github.com>
2013-11-16 22:48:00 +0200

    ukkonen's implementation is compilable and loadable, needs good debug

---
+++ t/smoke.lisp
@@ -0,0 +1,77 @@
+;;; -*- package: CL-STRING-MATCH-TEST; Syntax: Common-lisp; Base: 10 -*-
+
+
+;; Running tests from the command line:
+;; 
+;; sbcl --load smoke.lisp --eval '(test:run)' --quit
+;; lx86cl --load smoke.lisp --eval '(test:run)' --eval '(quit)'
+
+;; --------------------------------------------------------
+
+
+(in-package :cl-string-match-test)
+
+(setq *print-failures* t)
+
+;; --------------------------------------------------------
+
+(defparameter *funcs*
+  '(sm:string-contains-brute
+    sm:string-contains-bm
+    sm:string-contains-bmh
+    sm:string-contains-rk
+    sm:string-contains-kmp
+    sm:string-contains-ac))
+
+;; --------------------------------------------------------
+
+(defmacro run-assertions (val needle haystack)
+  `(progn ,@(loop :for func :in *funcs*
+	       :collect `(assert-equal ,val (,func ,needle ,haystack)))))
+
+;; --------------------------------------------------------
+
+(define-test basic-test
+  (run-assertions 0 "a" "a--")
+  (run-assertions 1 "a" "-a-")
+  (run-assertions 2 "a" "--a")
+  (run-assertions nil "a" "-b-"))
+
+;; --------------------------------------------------------
+
+(define-test str-test
+  (run-assertions 0 "abc" "abcab_")
+  (run-assertions 1 "abc" "_abcab_")
+  (run-assertions 2 "abc" "ababc"))
+
+;; --------------------------------------------------------
+
+(define-test ac-test
+    ;; test Aho-Corasick implementation how it deals with multiple
+    ;; patterns search
+
+    (let ((trie (initialize-ac '("he" "she" "his" "hers"))))
+      (assert-equal 0 (search-ac trie "she"))
+      (assert-equal 1 (search-ac trie "_she"))
+      (assert-equal nil (search-ac trie "_sh_"))
+      
+      (multiple-value-bind (pos idx)
+	  (search-ac trie "___his")
+	(assert-equal 3 pos)
+	(assert-equal 2 idx))
+      (multiple-value-bind (pos idx)
+	  (search-ac trie "___h_s")
+	(assert-equal nil pos)
+	(assert-equal nil idx))))
+
+;; --------------------------------------------------------
+
+(define-test ukk-tree
+    )
+
+;; --------------------------------------------------------
+
+(defun run ()
+  (lisp-unit:run-all-tests :cl-string-match-test))
+
+;; EOF
